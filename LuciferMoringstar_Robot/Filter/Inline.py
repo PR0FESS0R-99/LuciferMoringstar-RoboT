db6fc258e7b0ad6fec413e6a8fd34352b0cc76c9
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-09-29 14:10:05 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,10 +89,10 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Support Group', url='t.me/Mo_Tech_Group'),
-        InlineKeyboardButton('More Botz', url='t.me/MT_Botz')
+        InlineKeyboardButton('Support Group', url='https://t.me/KicchaRequest'),
+        InlineKeyboardButton('File Channel', url='https://t.me/GD_FILMCLUB')
         ],[
-        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+        InlineKeyboardButton('Tv Series', url=f'https://t.me/TV_VIRISION')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
