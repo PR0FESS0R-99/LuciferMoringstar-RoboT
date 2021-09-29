db56e0c8b2d878db540e09f88c19fb1e5c65e8d9
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-09-29 22:57:00 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -353,4 +353,4 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "pages":
             await query.answer()
     else:
-        await query.answer("കൌതുകും ലേശം കൂടുതൽ ആണല്ലേ👀",show_alert=True)
+        await query.answer("Nice Try With You Don't Asking Me👀",show_alert=True)
