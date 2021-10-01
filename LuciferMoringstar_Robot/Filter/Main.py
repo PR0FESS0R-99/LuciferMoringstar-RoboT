58c58562f82a9bb66fbb63d70dda83b025649a71
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-10-01 11:24:09 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -275,7 +275,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     InlineKeyboardButton('🗣️ Group', url=f'https://t.me/KicchaRequest')
                 ],
                 ]
-                    InlineKeyboardButton("About 🤠", callback_data="about"),
+                    InlineKeyboardButton("About 🤠", callback_data="about")
                 ]
                 ]
             await query.message.edit(text=f"{HELP}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
@@ -287,7 +287,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     InlineKeyboardButton('🗣️ Group', url=f'https://t.me/KicchaRequest')
                 ],
                 [
-                    InlineKeyboardButton("🤔 Help", callback_data="help"),
+                    InlineKeyboardButton("🤔 Help", callback_data="help")
                 ]
                 ]
             await query.message.edit(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
