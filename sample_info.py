c9b3faa1abd5ff7d017e26c3eefaa8dcdd87cd2e
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-10-03 20:42:32 +0530

    Add files via upload

---
+++ sample_info.py
@@ -0,0 +1,36 @@
+# Bot information
+SESSION = 'LuciferMoringstar_Robot'
+USER_SESSION = 'User_Bot'
+API_ID = 12345
+API_HASH = '0123456789abcdef0123456789abcdef'
+BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
+USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [12345789, 'admin123', 987654321]
+CHANNELS = [-10012345678, -100987654321, 'Mo_Tech_YT']
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb://[LuciferMoringstar_Robot:LuciferMoringstar_Robot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_NAME = 'LuciferMoringstar_Robot'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+# Messages
+START_MSG = """<b>👋𝙷𝚎𝚕𝚕𝚘 {},
+
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 [ 𝐀𝐋𝐋𝐔 𝐀𝐑𝐉𝐔𝐍 𝐑𝐎𝐁𝐎𝐓](https://t.me/Allu_ArjunROBOT)  𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂 𝙸𝙽 𝙼𝚈 𝙶𝚁𝙾𝚄𝙿 𝙾𝙽𝙻𝚈,
+𝙸𝚃'𝚂 𝚅𝙴𝚁𝚈 𝙴𝙰𝚂𝚈. 𝙹𝚄𝚂𝚃 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚂𝙴𝙰𝚁𝙲𝙷 𝙼𝙾𝚅𝙸𝙴 𝙸𝙽 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙱𝙾𝚃 𝙸 𝚆𝙴𝙻𝙻 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂 𝚃𝙷𝙴𝚁𝙴... 
+
+𝚃𝚑𝚊𝚗𝚔 𝚈𝚘𝚞 𝙵𝚘𝚛 𝚁𝚎𝚚𝚞𝚎𝚜𝚝 𝙼𝚘𝚟𝚒𝚎𝚜 𝙰𝚗𝚢 𝙿𝚛𝚘𝚋𝚕𝚎𝚖 𝙲𝚘𝚗𝚝𝚊𝚌𝚝 𝙼𝚢 𝙱𝚘𝚜𝚜 𝙰𝚗𝚍 𝚁𝚎𝚚𝚞𝚎𝚜𝚝...😝
+
+𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍 𝙱𝚢 𝙼𝚢 𝙱𝚘𝚜𝚜 ☛[𝑺𝒂𝒄𝒉𝒊𝒏 𝑺](https://t.me/sachin_official_admin)
+"""
+
+SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
+INVITE_MSG = 'Please join @.... to use this bot'
