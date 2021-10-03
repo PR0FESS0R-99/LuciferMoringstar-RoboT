766c77a98ca1b22ab357f1437c2a22d0580c11f2
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-10-03 13:12:20 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -118,7 +118,7 @@ async def start(bot, message):
         )
     else:
         await message.reply_text(
-            START_MSG,
+            text=Script.START_MSG.format(message.from_user.mention),
             parse_mode="Markdown",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
