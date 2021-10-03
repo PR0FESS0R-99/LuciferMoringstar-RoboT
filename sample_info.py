890c68d017f57363390dcd51d36f37edd15a3f0b
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-10-03 20:14:57 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,24 +1,24 @@
 # Bot information
-SESSION = 'LuciferMoringstar_Robot'
+SESSION = 'Allu_ArjunROBOT'
 USER_SESSION = 'User_Bot'
 API_ID = 12345
 API_HASH = '0123456789abcdef0123456789abcdef'
 BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
+USERBOT_STRING_SESSION = 'Allu_ArjunROBOT'
 
 # Bot settings
 CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 987654321]
-CHANNELS = [-10012345678, -100987654321, 'Mo_Tech_YT']
+ADMINS = [123456789, 'admin123', 987654321]
+CHANNELS = [-1001483336810, -1001280344929, 'GD_FILMCLUB']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[LuciferMoringstar_Robot:LuciferMoringstar_Robot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'LuciferMoringstar_Robot'
+DATABASE_URI = "mongodb://[Allu_ArjunROBOT:Allu_ArjunROBOT@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_NAME = 'Allu_ArjunROBOT'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
