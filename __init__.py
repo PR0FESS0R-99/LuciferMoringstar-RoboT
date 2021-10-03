32dc99dfdb781689333414c9cb33de0f121203c0
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-10-03 20:45:02 +0530

    Allu_ArjunROBOT

---
+++ __init__.py
@@ -0,0 +1,57 @@
+from .Utils import (
+   get_filter_results,
+   get_file_details,
+   is_subscribed,
+   get_poster,
+   Media
+)
+from .Channel import (
+   RATING,
+   GENRES
+)
+
+HELP = """
+○  it's Note Complicated...🤓
+
+○  Search using inline mode
+This methord works on any chat, Just type @Allu_ArjunROBOT and then leave a space and search any movie you want...
+
+○ Available Commands
+     
+ Basic Commads
+
+/start :- check am Alive
+
+Bot Owner Only
+
+/broadcast Replay Any Message or Media
+/stats user Status
+/ban_user  click ban_user more info
+/unban_user click unban_user more info
+/banned_users Banned User Details
+/total How Many Files Added In Database
+/logger  Get Logs
+/delete Delete File From Database
+
+○ Notice 📙:-
+
+○Dont Spam Me...🤒
+
+©️MᴀɪɴᴛᴀɪɴᴇD Bʏ :- [SACHIN S](https://t.me/sachin_official_admin)
+
+@KicchaRequest
+"""
+
+ABOUT = """
+➥ Name :  [Allu Arjun RoBot](https://t.me/Allu_ArjunROBOT)
+    
+➥ Creator : [Sachin S](https://t.me/sachin_official_admin)
+
+➥ Language : Python3
+
+➥ Library : Pyrogram Asyncio 1.13.0
+
+➥ Request Group : [Kiccha Request](https://t.me/KicchaRequest)
+
+➥ Movie Channel :  [KICCHA REQUEST Channel](https://t.me/GD_FILMCLUB)
+"""
