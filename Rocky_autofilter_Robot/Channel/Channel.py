c07f1742c1144e50513a4a9510a2d2725e710df4
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-11-05 13:16:36 +0530

    Update Channel.py

--- Rocky_autofilter_Robot/Channel/Channel.py
+++ Rocky_autofilter_Robot/Channel/Channel.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from LuciferMoringstar_Robot.Utils import save_file
+from Rocky_autofilter_Robot.Utils import save_file
 from Config import CHANNELS
 
 media_filter = filters.document | filters.video | filters.audio
