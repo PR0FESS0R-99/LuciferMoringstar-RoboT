402a866fb17b5bcd2300782027fcea18ea5aa52b
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-11-07 22:04:46 +0530

    Update Commands.py

--- Rocky_autofilter_Robot/Commands.py
+++ Rocky_autofilter_Robot/Commands.py
@@ -1,7 +1,7 @@
 import os
 import logging
 import random
-import script
+from Script import script
 from pyrogram import Client, filters
 from pyrogram import StopPropagation
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
