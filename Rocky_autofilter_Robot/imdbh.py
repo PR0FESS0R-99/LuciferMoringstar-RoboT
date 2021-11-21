fcbde29288625e9ae35713f8e5adf3b704282260
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-11-21 10:41:18 +0530

    Add files via upload

---
+++ Rocky_autofilter_Robot/imdbh.py
@@ -0,0 +1,21 @@
+
+import random
+import logging
+
+async def save_poster(imdb_id, title, year, url):
+    try:
+        data = Poster(
+            imdb_id=imdb_id,
+            title=title,
+            year=int(year),
+            poster=url
+        )
+    except ValidationError:
+        logger.exception('Error occurred while saving poster in database')
+    else:
+        try:
+            await data.commit()
+        except DuplicateKeyError:
+            logger.warning("already saved in database")
+        else:
+            logger.info("Poster is saved in database")
