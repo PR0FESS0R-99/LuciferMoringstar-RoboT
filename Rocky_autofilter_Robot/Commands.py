307b61c126fec4cb0cf33234ec03f25df23f0030
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-11-21 10:40:32 +0530

    Update Commands.py

--- Rocky_autofilter_Robot/Commands.py
+++ Rocky_autofilter_Robot/Commands.py
@@ -6,10 +6,10 @@ from pyrogram import Client, filters
 from pyrogram import StopPropagation
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from Config import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, TUTORIAL, BROADCAST_CHANNEL, DB_URL, SESSION, ADMIN_ID    
-from LuciferMoringstar_Robot.Utils import Media, get_file_details 
-from LuciferMoringstar_Robot.Broadcast import broadcast
-from LuciferMoringstar_Robot import ABOUT
-from LuciferMoringstar_Robot.Channel import handle_user_status
+from Rocky_autofilter_Robot.Utils import Media, get_file_details 
+from Rocky_autofilter_Robot.Broadcast import broadcast
+from Rocky_autofilter_Robot import ABOUT
+from Rocky_autofilter_Robot.Channel import handle_user_status
 from Database import Database
 from pyrogram.errors import UserNotParticipant
 logger = logging.getLogger(__name__)
