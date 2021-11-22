7432ec768e5a4856e94145d7e30db3a7889e78aa
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-11-22 11:12:30 +0530

    Update Main.py

--- Rocky_autofilter_Robot/Filter/Main.py
+++ Rocky_autofilter_Robot/Filter/Main.py
@@ -6,7 +6,7 @@ from OMDB import get_movie_info
 import re
 from pyrogram.errors import UserNotParticipant
 from Rocky_autofilter_Robot import get_filter_results, get_file_details, is_subscribed, get_poster
-from Rocky_autofilter_Robot import RATING, GENRES, RELEASE_INFO, HELP, ABOUT, imdb, add_filter, find_filter, get_filters, delete_filter, count_filters, Database, active_connection, add_user, all_users,  parser, split_quotes, IMDBCONTROL, google_search  
+from Rocky_autofilter_Robot import RATING, GENRES, RELEASE_INFO, HELP, ABOUT
 import random
 import asyncio
 BUTTONS = {}
