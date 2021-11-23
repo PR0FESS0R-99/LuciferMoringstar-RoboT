f908ca87e00d8a73fb2c3929dde8e4e024113b0f
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-11-23 20:50:12 +0530

    Update Main.py

--- Rocky_autofilter_Robot/Filter/Main.py
+++ Rocky_autofilter_Robot/Filter/Main.py
@@ -142,7 +142,7 @@ async def group(client, message):
                 file_id = file.file_id
                 filename = f"{file.file_name}"
                 btn.append(
-                    [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=sachin9742s#{file_id}"),
+                    [InlineKeyboardButton(text=f"{filename}", url=f"https://telegram.dog/{nyva}?start=sachin9742s_-_-_-_{file_id}"),
                      InlineKeyboardButton(text=f"{get_size(file.file_size)}", callback_data=f'files_#{file.file_id}',)]
                 )
         else:
