ac32a4c6a9a0e71c7bae765baf3be8e8511ffb24
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-11-26 22:06:41 +0530

    Add files via upload

--- Rocky_autofilter_Robot/paulwalker_tg/arrow.py
+++ Rocky_autofilter_Robot/paulwalker_tg/arrow.py
@@ -6,22 +6,22 @@ from info import COMMAND_HAND_LER
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 # EMOJI CONSTANTS
-GOAL_E_MOJI = "⚽"
+DART_E_MOJI = "🎯"
 # EMOJI CONSTANTS
 
 
 @Client.on_message(
-    filters.command(["goal", "shoot"], COMMAND_HAND_LER) &
+    filters.command(["throw", "dart"], COMMAND_HAND_LER) &
     f_onw_fliter
 )
-async def roll_dice(client, message):
-    """ @Goal """
+async def throw_dart(client, message):
+    """ /throw an @AnimatedDart """
     rep_mesg_id = message.message_id
     if message.reply_to_message:
         rep_mesg_id = message.reply_to_message.message_id
     await client.send_dice(
         chat_id=message.chat.id,
-        emoji=GOAL_E_MOJI,
+        emoji=DART_E_MOJI,
         disable_notification=True,
         reply_to_message_id=rep_mesg_id
     )
