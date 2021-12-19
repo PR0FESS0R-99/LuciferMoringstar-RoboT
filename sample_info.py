53c306e57fd456934e471f58e9c0caa3f9859bd9
SDBUB7 <94208123+SDBUB7@users.noreply.github.com>
2021-12-19 08:40:55 +0530

    File ssearch button and Start Massage

--- sample_info.py
+++ sample_info.py
@@ -25,8 +25,5 @@ COLLECTION_NAME = 'channel_files'  # If you are using the same database, then us
 START_MSG = """
 **Hi, I'm Media Search bot**
 
-Here you can search files in inline mode. Just press follwing buttons and start searching.
-"""
-
-SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
+Developer= @SDBUB7 
 INVITE_MSG = 'Please join @.... to use this bot'
