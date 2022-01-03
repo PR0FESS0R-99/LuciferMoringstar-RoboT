440686692bb0029e491cb2291cddb1cd0be8cebc
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-01-03 18:13:55 +0530

    bot UserName Added

--- mt_botz.py
+++ mt_botz.py
@@ -8,7 +8,7 @@ logging.getLogger().setLevel(logging.ERROR)
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from LuciferMoringstar_Robot import Media
-from Config import SESSION, API_ID, API_HASH, BOT_TOKEN
+from Config import SESSION, API_ID, API_HASH, BOT_TOKEN, BOT_USERNAME
 import pyromod.listen
 
 class Bot(Client):
@@ -29,7 +29,7 @@ class Bot(Client):
         await Media.ensure_indexes()
         me = await self.get_me()
         self.username = '@' + me.username
-        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
+        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on @{BOT_USERNAME}.")
 
     async def stop(self, *args):
         await super().stop()
