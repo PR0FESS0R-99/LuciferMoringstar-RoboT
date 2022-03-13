3441469bc1a634460ed879a8a83c5ce48e5afa87
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-13 18:36:17 +0530

    Fixed

--- LuciferMoringstar_Robot/autofilter/Index.py
+++ LuciferMoringstar_Robot/autofilter/Index.py
@@ -143,7 +143,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
         try:
             current = CURRENT
             CANCEL = False
-            async for message in bot.iter_messages(chat, lst_msg_id, temp.CURRENT):
+            async for message in bot.iter_messages(chat, lst_msg_id, CURRENT):
                 if CANCEL:
                     await msg.edit(f"Successfully Cancelled!!\n\nSaved <code>{total_files}</code> files to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>")
                     break
