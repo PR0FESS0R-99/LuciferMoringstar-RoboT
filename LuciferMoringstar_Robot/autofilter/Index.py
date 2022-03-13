fa811a7ab052fb7682e690a298498a9431138fd0
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-13 18:19:54 +0530

    Index Saving Problem Fixed

--- LuciferMoringstar_Robot/autofilter/Index.py
+++ LuciferMoringstar_Robot/autofilter/Index.py
@@ -1,14 +1,12 @@
 # https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/index.py
-# (Old Code) Thanks To EvamariaTG 
-import logging, os, asyncio
+# (New Code) Thanks To EvamariaTG 
+import logging, asyncio, re
 from pyrogram import Client as LuciferMoringstar_Robot, filters as Worker
 from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, ChatAdminRequired, UsernameInvalid, UsernameNotModified
 from config import ADMINS, LOG_CHANNEL, CURRENT, CANCEL
 from LuciferMoringstar_Robot.database.autofilter_db import save_file
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-import re
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 lock = asyncio.Lock()
@@ -88,7 +86,7 @@ async def send_for_index(bot, message):
                                      callback_data=f'index#accept#{chat_id}#{last_msg_id}#{message.from_user.id}')
             ],
             [
-                InlineKeyboardButton('close', callback_data='close'),
+                InlineKeyboardButton('close', callback_data='close_data'),
             ]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -128,7 +126,7 @@ async def set_skip_number(bot, message):
             skip = int(skip)
         except:
             return await message.reply("Skip number should be an integer.")
-        await message.reply(f"Succesfully set SKIP number as {skip}")
+        await message.reply(f"Successfully set SKIP number as {skip}")
         CURRENT = int(skip)
     else:
         await message.reply("Give me a skip number")
@@ -140,61 +138,47 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
     errors = 0
     deleted = 0
     no_media = 0
+    unsupported = 0
     async with lock:
-        try: 
-            total = lst_msg_id + 1
+        try:
             current = CURRENT
             CANCEL = False
-            while current < total:
+            async for message in bot.iter_messages(chat, lst_msg_id, temp.CURRENT):
                 if CANCEL:
-                    await msg.edit("Succesfully Cancelled")
+                    await msg.edit(f"Successfully Cancelled!!\n\nSaved <code>{total_files}</code> files to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>")
                     break
-                try:
-                    message = await bot.get_messages(chat_id=chat, message_ids=current, replies=0)
-                except FloodWait as e:
-                    await asyncio.sleep(e.x)
-                    message = await bot.get_messages(
-                        chat,
-                        current,
-                        replies=0
-                    )
-                except Exception as e:
-                    logger.exception(e)
-                try:
-                    for file_type in ("document", "video", "audio"):
-                        media = getattr(message, file_type, None)
-                        if media is not None:
-                            break
-                        else:
-                            continue
-                    media.file_type = file_type
-                    media.caption = message.caption
-                    aynav, vnay = await save_file(media)
-                    if aynav:
-                        total_files += 1
-                    elif vnay == 0:
-                        duplicate += 1
-                    elif vnay == 2:
-                        errors += 1
-                except Exception as e:
-                    if "NoneType" in str(e):
-                        if message.empty:
-                            deleted += 1
-                        elif not media:
-                            no_media += 1
-                        logger.warning("Skipping deleted / Non-Media messages (if this continues for long, use /setskip to set a skip number)")     
-                    else:
-                        logger.exception(e)
                 current += 1
                 if current % 20 == 0:
                     can = [[InlineKeyboardButton('Cancel', callback_data='index_cancel')]]
                     reply = InlineKeyboardMarkup(can)
                     await msg.edit_text(
-                        text=f"Total messages fetched: <code>{current}</code>\nTotal messages saved: <code>{total_files}</code>\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media}</code>\nErrors Occured: <code>{errors}</code>",
+                        text=f"Total messages fetched: <code>{current}</code>\nTotal messages saved: <code>{total_files}</code>\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>",
                         reply_markup=reply)
+                if message.empty:
+                    deleted += 1
+                    continue
+                elif not message.media:
+                    no_media += 1
+                    continue
+                elif message.media not in ['audio', 'video', 'document']:
+                    unsupported += 1
+                    continue
+                media = getattr(message, message.media, None)
+                if not media:
+                    unsupported += 1
+                    continue
+                media.file_type = message.media
+                media.caption = message.caption
+                aynav, vnay = await save_file(media)
+                if aynav:
+                    total_files += 1
+                elif vnay == 0:
+                    duplicate += 1
+                elif vnay == 2:
+                    errors += 1
         except Exception as e:
             logger.exception(e)
             await msg.edit(f'Error: {e}')
         else:
-            await msg.edit(f'Succesfully saved <code>{total_files}</code> to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media}</code>\nErrors Occured: <code>{errors}</code>')
+            await msg.edit(f'Succesfully saved <code>{total_files}</code> to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>')
 
