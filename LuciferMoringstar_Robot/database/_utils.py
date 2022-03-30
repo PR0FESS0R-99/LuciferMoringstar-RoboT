e62dfb633bd2632eae1951b95dd05c2c96f7bcee
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-03-30 17:22:22 +0530

    New Updated

--- LuciferMoringstar_Robot/database/_utils.py
+++ LuciferMoringstar_Robot/database/_utils.py
@@ -1,6 +1,6 @@
 import logging
 
-from pyrogram.errors import UserNotParticipant
+from pyrogram.errors import UserNotParticipant, FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -38,3 +38,21 @@ async def is_subscribed(bot, query):
             return True
 
     return False
+
+# ~~~~ Broadcast Message ~~~~ #
+
+async def send_msg(user_id, message):
+    try:
+        await message.copy(chat_id=user_id)
+        return 200, None
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        return send_msg(user_id, message)
+    except InputUserDeactivated:
+        return 400, f"{user_id} : deactivated\n"
+    except UserIsBlocked:
+        return 400, f"{user_id} : blocked the bot\n"
+    except PeerIdInvalid:
+        return 400, f"{user_id} : user id invalid\n"
+    except Exception as e:
+        return 500, f"{user_id} : {traceback.format_exc()}\n"
