1967051571e987ff3a247b5cba50b4c08d1b5e86
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2022-05-11 19:44:35 +0530

    Update logger.py

--- Rocky_autofilter_Robot/autofilter/logger.py
+++ Rocky_autofilter_Robot/autofilter/logger.py
@@ -1,10 +1,10 @@
 import logging
-from pyrogram import Client as LuciferMoringstar_Robot, filters as Worker
+from pyrogram import Client as Rocky_autofilter_Robot, filters as Worker
 from config import ADMINS
 logger = logging.getLogger(__name__)
 
 
-@LuciferMoringstar_Robot.on_message(Worker.command('logger') & Worker.user(ADMINS))
+@Rocky_autofilter_Robot.on_message(Worker.command('logger') & Worker.user(ADMINS))
 async def log_file(bot, message):
     """Send log file"""
     try:
