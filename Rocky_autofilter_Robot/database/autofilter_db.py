a5a1ae24f0b60eda2ab2123a310566d73a8a7f33
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2022-05-12 00:34:38 +0530

    Update autofilter_db.py

--- Rocky_autofilter_Robot/database/autofilter_db.py
+++ Rocky_autofilter_Robot/database/autofilter_db.py
@@ -6,7 +6,7 @@ from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
-from config import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, FORCES_SUB
+from import DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, FORCES_SUB
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 from imdb import IMDb
