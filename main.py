ccc25832fb23969c03ac86555c97e0261a3f155e
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-07-13 19:29:17 +0530

    IMDB Support Added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging.config
 logging.config.fileConfig('logger.file')
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-
+logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
