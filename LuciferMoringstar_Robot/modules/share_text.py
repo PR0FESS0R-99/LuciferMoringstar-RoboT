2a7f20d374e02f69a611d70d2e5cc4e76ba03969
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-07-23 13:11:19 +0530

    Fixed ShareText 🎉

--- LuciferMoringstar_Robot/modules/share_text.py
+++ LuciferMoringstar_Robot/modules/share_text.py
@@ -7,7 +7,7 @@ from pyrogram import *
 async def sharelink(bot, update):
     # https://github.com/PR0FESS0R-99/ShareText-Bot/blob/01a849d3cdb6220509b8883e47df2e64e704ae55/main.py#L17 
     if len(update.command) != 2:
-        await update.reply("**--Use Correct Format-- :-\n  • `/sharetext your text`**")
+        return await update.reply("**--Use Correct Format-- :-\n  • `/sharetext your text`**")
     await bot.send_photo(chat_id=update.chat.id,
         caption=f"**Message Sharing Link Is Ready** :- https://t.me/share/url?url={quote(update.text)}", reply_to_message_id=update.id, reply_markup=InlineKeyboardMarkup( [[ InlineKeyboardButton("📤 Share Link 📤", url=f"https://t.me/share/url?url={quote(update.text)}") ]] )       
     )
