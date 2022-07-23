6f4afbfc65bbb29a1a12bc8568231094be956765
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-07-23 13:12:45 +0530

    Command Fix

--- LuciferMoringstar_Robot/modules/share_text.py
+++ LuciferMoringstar_Robot/modules/share_text.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import *
 from pyrogram import *
 
-@Client.on_message(filters.private & filters.command(["sharetext"]))
+@Client.on_message(filters.private & filters.command(["share"]))
 async def sharelink(bot, update):
     # https://github.com/PR0FESS0R-99/ShareText-Bot/blob/01a849d3cdb6220509b8883e47df2e64e704ae55/main.py#L17 
     if len(update.command) != 2:
