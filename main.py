59681b1ff7f6fe4c01a7848a074b093241ce16ad
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-10-20 17:39:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "LuciferMoringstar_Robot",
+            "LuciferMoringstar-Robot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
