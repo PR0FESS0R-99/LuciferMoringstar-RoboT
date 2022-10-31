b2734366069acf62206470f7a766ba53b8c18ba2
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-10-31 16:28:55 +0530

    Delete LuciferMoringstar-Robot directory

--- LuciferMoringstar-Robot/plugins/command.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-from LuciferMoringstar-Robot.plugins.work import message
-
-@Client.on_message(filters.command("start"))
-async def start_command(bot, update):
-    await update.reply("Bot Started")
-    await message(update)
-    
--- LuciferMoringstar-Robot/plugins/work.py
+++
@@ -1,2 +0,0 @@
-async def message(message):
-    await message.reply("h")
