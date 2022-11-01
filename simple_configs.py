f7a366f0efd8f44bf521ae3fd0b4ca23befb2895
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-01 22:04:16 +0530

    Update simple_configs.py

--- simple_configs.py
+++ simple_configs.py
@@ -4,3 +4,8 @@ class Config(object):
     TG_API_ID = int(environ['API_ID'])
     TG_API_HASH = environ['API_HASH']
     TG_BOT_TOKEN = environ['BOT_TOKEN']
+
+class Bots(object):
+    BOT_ID = None
+    BOT_NAME = "Lucifer"
+    BOT_USERNAME = "Midukki_RoboT"
