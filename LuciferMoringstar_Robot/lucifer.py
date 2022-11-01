157fecd63ef7c5717c65fb5d63b3e37aa919f8d8
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-01 16:43:34 +0530

    Update lucifer.py

--- LuciferMoringstar_Robot/lucifer.py
+++ LuciferMoringstar_Robot/lucifer.py
@@ -5,26 +5,12 @@
 from pyrogram import Client, enums, __version__
 from simple_configs import Config
 
-class lucifermoringstar_robot(Client):
-
-    def __init__(self):
-        super().__init__(
-            "LuciferMoringstar-Robot",
-            api_hash=Config.TG_API_HASH,
-            api_id=Config.TG_API_ID,
-            plugins={
-                "root": "./plugins"
-            },
-            workers=200,
-            bot_token=Config.TG_BOT_TOKEN,
-            sleep_threshold=10
-        )
-
-    async def start(self):
-        await super().start()
-        bot_details = await self.get_me()
-        print("Bot Start")
-        
-    async def stop(self, *args):
-        await super().stop()
-        print("Bot stopped. Bye.")
+lucifermoringstar_robot = Client(
+  name="LuciferMoringstar-Robot",
+  api_hash=Config.TG_API_HASH,
+  api_id=Config.TG_API_ID,
+  bot_token=Config.TG_BOT_TOKEN,
+  plugins={"root": "LuciferMoringstar_Robot/plugins"},
+  sleep_threshold=10,
+  workers=200
+)
