5fbcb6785328d06053da5c09bd3aebf30e2dbb36
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-01 22:06:34 +0530

    Update simple_configs.py

--- simple_configs.py
+++ simple_configs.py
@@ -6,6 +6,6 @@ class Config(object):
     TG_BOT_TOKEN = environ['BOT_TOKEN']
 
 class Bots(object):
-    BOT_ID = None
-    BOT_NAME = "Lucifer"
-    BOT_USERNAME = "Midukki_RoboT"
+    BOT_ID = int(environ.get('BOT_ID', None))
+    BOT_NAME = environ.get('BOT_NAME', 'Lucifer')
+    BOT_USERNAME = environ.get('BOT_USERNAME', 'Midukki_RoboT')
