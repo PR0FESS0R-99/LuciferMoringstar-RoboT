42419dee16375aac02fa61e103d2028e696abddf
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-01 22:22:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,30 +6,16 @@ from pyrogram.raw.all import layer
 from pyrogram import Client, __version__
 from simple_configs import Config
 
-class Lucifer(Client):
+Lucifermoringstar_Robot = Client(
+  name="lucifer-bot",
+  api_hash=Config.TG_API_HASH,
+  api_id=Config.TG_API_ID,
+  bot_token=Config.TG_BOT_TOKEN
+)
 
-    def __init__(self):
-        super().__init__(
-            name="lucifer-bot",
-            plugins={"root": "LuciferMoringstar_Robot"},
-            api_hash=Config.TG_API_HASH,
-            api_id=Config.TG_API_ID,
-            bot_token=Config.TG_BOT_TOKEN,
-            sleep_threshold=60,
-            workers=50
-        )
+print(f"""
+🤖 Lucifermoringstar-Robot based on Pyrogram v{__version__}
+(Layer {layer}) started on @{usr_bot_me.username}    
+""")
 
-    async def start(self):
-        await super().start()
-        usr_bot_me = self.me
-        print(
-            f"🤖 Lucifermoringstar-Robot based on Pyrogram v{__version__}"
-            f"(Layer {layer}) started on @{usr_bot_me.username}. "       
-        )
-
-    async def stop(self, *args):
-        await super().stop()
-        print("🤖 Lucifermoringstar-Robot stopped. Bye.")
-
-app = Lucifer()
-app.run()
+Lucifermoringstar_Robot.run()
