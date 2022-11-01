496fd5b49b92326c83e7654878f102dd87b2bf2b
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-01 16:44:50 +0530

    Update commands.py

--- LuciferMoringstar_Robot/plugins/commands.py
+++ LuciferMoringstar_Robot/plugins/commands.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
+from LuciferMoringstart_Robot.lucifer import lucifermoringstar_robot
 
-@Client.on_message(filters.command("start"))
+@lucifermoringstar_robot.on_message(filters.command("start"))
 async def start_command(bot, update):
     await update.reply("Bot Started")
